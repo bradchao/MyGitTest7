41ece1895af0231be6c6c3c3b1670a7848710eb6
raymond-mac <cy66380@chyi.com.tw>
2017-09-28 15:53:45 +0800

    CY66380

--- app/src/main/java/tw/brad/mygittest7/MainActivity.java
+++ app/src/main/java/tw/brad/mygittest7/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("brad", "V1");
+        Log.i("brad", "CY66380");
     }
 }
